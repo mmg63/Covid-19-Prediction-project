9082753f9d3223cf895cd38142a77c7b2394b96e
Mustafa Muhammadi <mustafa.mohammadi@ut.ac.ir>
2021-09-02 17:21:40 +0430

    read the .csv file

--- main.py
+++ main.py
@@ -10,4 +10,17 @@ import numpy as np
 import matplotlib.pyplot as pl
 import torch.nn as nn
 import torchvision
-import csv as csv
+import csv
+
+filepath = 'Covid-19_dataset/countermeasures_db_johnshopkins_2020_03_30.csv'
+with open(filepath) as csv_file:
+    csv_reader = csv.reader(csv_file, dialect=',')
+    line_count = 0
+    for row in csv_reader:
+        print(f'Column names are {", ".join(row)}')
+        line_count += 1
+    else:
+        print(f'\t{row[0]} works in the {row[1]} department, and was born in {row[2]}.')
+        line_count += 1
+    print(f'Processed {line_count} lines.')
+
