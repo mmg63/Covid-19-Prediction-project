b4becaebca7e88366e1526c62c45803b16a25b4d
Mustafa Muhammadi <mustafa.mohammadi@ut.ac.ir>
2021-09-02 17:10:49 +0430

    Initial commit

---
+++ main.py
@@ -0,0 +1,13 @@
+'''
+This code is implemented by Mustafa Mohammadi, graduate student of Mater degree from University of Tehran.
+
+This code is for Covid-19 classification
+There has been a dataset from the kaggle website
+
+'''
+
+import numpy as np
+import matplotlib.pyplot as pl
+import torch.nn as nn
+import torchvision
+import csv as csv
