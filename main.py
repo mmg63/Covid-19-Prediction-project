aea874aa17e95d3275ebf6f8f1c385f42048c334
Mustafa Muhammadi <mustafa.mohammadi@ut.ac.ir>
2021-09-06 09:26:36 +0430

    read the .csv file

--- main.py
+++ main.py
@@ -10,10 +10,13 @@ import numpy as np
 import matplotlib.pyplot as pl
 import torch.nn as nn
 import torchvision
+import torch
 import csv
 
 # filepath = 'Covid-19_dataset/countermeasures_db_johnshopkins_2020_03_30.csv'
-filepath = 'Covid-19_dataset/COVID 19 Containment measures data.csv'
+filepath = 'Covid-19_dataset/countermeasures_db_johnshopkins_2020_03_30.csv'
+# get data from csv and transmitted it into torch array
+data_array_of_lists = np.empty((10000, 29), dtype=object)
 with open(filepath, mode='r') as csv_file:
     csv_dictionary = csv.DictReader(csv_file)
     print('fields\' name:')
@@ -22,9 +25,16 @@ with open(filepath, mode='r') as csv_file:
     line_count = 0
     for row in csv_reader:
         print(f'Column names are {", ".join(row)}')
+        data_array_of_lists[line_count] = row
+        print(len(data_array_of_lists))
         line_count += 1
     else:
-        print(f'\t{row[0]} works in the {row[1]} department, and was born in {row[2]}.')
-        line_count += 1
-    print(f'Processed {line_count} lines.')
+        # Processed data from csv file and fit to an np array
+        csv_data = data_array_of_lists[0:line_count]
+        csv_headers = csv_dictionary.fieldnames
+
+        print(csv_data.shape)
+        del data_array_of_lists, row, line_count
+
+
 
